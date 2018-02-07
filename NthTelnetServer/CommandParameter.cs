a6d1d07ce8f68483ac920edeeb443f2cc4a36460
Mustafa Kok <mustafakok@gmail.com>
2018-02-07 20:37:20 +0300

    CommandPrameter is moved to a seperate file.

---
+++ NthTelnetServer/CommandParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NthDeveloper.TelnetServer
+{
+    public struct CommandParameter
+    {
+        public string Name { get; private set; }
+        public bool IsRequired { get; private set; }
+        public string Description { get; private set; }
+
+        public CommandParameter(string name, bool isRequired, string description)
+        {
+            this.Name = name;
+            this.IsRequired = isRequired;
+            this.Description = description;
+        }
+    }
+}
